48a419126e20146ba6fe8fc06b9b186dcf29c3f1
Ramunmi <raruznh@gmail.com>
2020-08-21 22:25:41 -0300

    [0.0.3] - 21/08/2020 - Middlewares

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,18 @@
+# [0.0.3] - 21/08/2020
+## Concept of Middleware
+## Improving routes
+## Inclusion of Uuid dependecy
+
+
+**Changed File**
+
+        modified:   Changelog.md
+        modified:   Semana 1/package.json
+        modified:   Semana 1/src/index.js
+        modified:   Semana 1/yarn.lock
+
+---
+
 # [0.0.2] - 21/08/2020
 ## Concepts about NODE JS, REST API, HTTP verbs and Params
 ## Creation of express app and routes
--- Semana 1/src/index.js
+++ Semana 1/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { uuid, isUuid } = require("uuidv4"); // Inportando apenas a fucao uuid da biblioteca uuidv4, ela tem como funcao criar um id unico universal
 
 const app = express();
 
@@ -20,18 +21,67 @@ app.use(express.json()); // necessario para que as rotas utilizem json
  *
  * Query params: Utilizados principamente para Filtros e paginacao, sao definidos apos um ? na url apos o recurso desejado. Esses parametros sao separados por &
  * Route Params: Utilizados para identificar recursos para identificar ou deletar, ele esta definido como um valor sem identificador apos o recurso desejado
- * Request body: Utilizado para envio de dados para o servidor, esse tipo de parametro nao deixara visivel qual os valores foram enviados para o servidor  
+ * Request body: Utilizado para envio de dados para o servidor, esse tipo de parametro nao deixara visivel qual os valores foram enviados para o servidor
  */
 
-let projetos = [];
+/**
+ * Middleware:
+ *
+ * Interceptador de requisiçoes, pode interromper totalmente uma requisicao
+ * Pode alterar dados de uma requisicao (por exemplo em um telefone sem fio)
+ * Ele tem o formato de funcao, recebem request e response, rotas sao middlewares
+ * Para middlewares que nao sao rotas teremos o parametro next para indicar qual sera a proxima rota a ser chamada
+ * Middlewares sao utilizados para disparar automaticamente trechos de codigo de uma requisicao
+ * É possivel incluir um middle em uma rota especifica, basta informar ele na chamada do metodo, antes da function que a rota devera executar
+ */
+
+let projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const loglabel = `[${method.toUpperCase()} ${url}]`;
+  console.time(loglabel);
+
+   next(); // deve ser chamada para que a rota seguinte aconteça
+
+  console.timeEnd(loglabel)
+}
+
+function validateProjectId(request,response,next){
+    const {id} = request.params;
+    if(!isUuid(id)){
+        return response.status(400).json({error: "Invalid ID!"})
+    }
+    return next()
+}
+
+app.use(logRequests); // definindo que o middleware deverá ser utilizado para todas as rotas, deve ser incluido antes das rotas
+
+app.use("/projects/:id",validateProjectId) // Dessa forma nao sera necessario colocar em cada rota, pois criamos um padrao de rota que esse middle devera ser aplicado
 
 app.get("/", (request, response) => {
   return response.send("Rota raiz"); // Retorna apenas um texto
 });
 
 app.get("/projects", (request, response) => {
-    const {title,owner} = request.query; // Como utilizar os parametros da querystring
-  return response.json(projetos);
+  const { title, owner } = request.query; // Como utilizar os parametros da querystring
+
+
+  let results = []
+
+   results.push(title
+    ? projects.filter((project) => project.title.includes(title)) 
+    :null );
+    
+    results.push(
+        owner ? projects.filter((project) => project.owner === owner) : null
+  );
+  
+ //if (results && results.length > 0) return response.json(results);
+
+
+  return response.json(projects);
 }); // Definindo a rota projects
 
 app.get("/json", (request, response) => {
@@ -39,20 +89,44 @@ app.get("/json", (request, response) => {
 });
 
 app.post("/projects", (request, response) => {
-  let { title,owner } = request.body;
-  projetos.push(title);
-  return response.json(projetos);
+  let { title, owner } = request.body;
+  let project = { id: uuid(), title, owner };
+  projects.push(project);
+  return response.json(project);
 });
 
 app.put("/projects/:id", (request, response) => {
-  let {id} = request.params;
-  console.log(id)
-  return response.json(projetos);
+  let { id } = request.params;
+  let { title, owner } = request.body;
+  let projectIndex = projects.findIndex((project) => {
+    return project.id === id;
+  });
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: "Project not found!" });
+  }
+
+  let project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
-app.delete("/projects/:id", (request, response) => {
-    let params = request.params;
-  return response.json(projetos);
+app.delete("/projects/:id", validateProjectId, (request, response) => {
+  let { id } = request.params;
+
+  let projectIndex = projects.findIndex((project) => {
+    return project.id === id;
+  }); // procura o index do projeto por id e caso nao encontre retorna o index -1
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: "Project not found!" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
